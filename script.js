ef8320fc46cce7905acc46200b6073485efe4588
Harvey Yang <yanghanji1@gmail.com>
2024-07-18 10:18:13 +0800

    基本文件结构

---
+++ script.js
@@ -0,0 +1,31 @@
+function add(a, b) {
+  return a + b;
+}
+function subtract(a, b) {
+  return a - b;
+}
+function multiply(a, b) {
+  return a * b;
+}
+function divide(a, b) {
+  return a / b;
+}
+
+let firstNumber;
+let operator;
+let secondNumber;
+
+function operate(firstNumber, operator, secondNumber) {
+  switch (operator) {
+    case "+":
+      return add(firstNumber, secondNumber);
+    case "-":
+      return subtract(firstNumber, secondNumber);
+    case "*":
+      return multiply(firstNumber, secondNumber);
+    case "/":
+      return divide(firstNumber, secondNumber);
+    default:
+      return "Invalid operator";
+  }
+}
